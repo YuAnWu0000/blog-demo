7f9286e68a3f8baf407c09dc9ab6bbf0fd810d0b
zachary <andywu03242000@gmail.com>
2024-07-04 09:15:05 +0800

    ADD: BandCircle component

---
+++ src/components/bandCircle/BandCircle.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+const list = new Array(20).fill(0)
+
+const BandCircle = () => {
+  return <div className={styles.container}></div>
+}
+
+export default BandCircle
