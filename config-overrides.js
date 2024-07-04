54c486fb18bcbbe62f4ef2fba2dcbf87d8b98391
zachary <andywu03242000@gmail.com>
2024-07-04 09:06:47 +0800

    Url issue

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,13 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = function override(config) {
-  config.resolve.alias = {
-    ...config.resolve.alias,
-    "@": path.resolve(__dirname, "./src"),
-  };
-  return config;
-};
+  const targetRegex = /\.css$/
+  const targetPackage = /css-loader/
+  // to make url() in css worked
+  config.module.rules
+    .find((r) => r.oneOf)
+    .oneOf.find((r) => r.test.toString() === targetRegex.toString())
+    .use.find((o) => o.loader && targetPackage.test(o.loader)).options.url =
+    false
+  return config
+}
