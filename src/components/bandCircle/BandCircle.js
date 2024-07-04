23785a256e1d16e91a51bb7d678e54a3356c311f
zachary <andywu03242000@gmail.com>
2024-07-04 09:16:41 +0800

    ADD: Left circle for BandCircle component

--- src/components/bandCircle/BandCircle.js
+++ src/components/bandCircle/BandCircle.js
@@ -4,7 +4,17 @@ import styles from './index.module.scss'
 const list = new Array(20).fill(0)
 
 const BandCircle = () => {
-  return <div className={styles.container}></div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.circle}>
+          {list.map((item, index) => {
+            return <span key={index} className={styles[`span${index}`]}></span>
+          })}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default BandCircle
