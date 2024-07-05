01496f52426f90db3e82c8f77ba4df163431381c
zachary <andywu03242000@gmail.com>
2024-07-05 13:53:53 +0800

    ADD: Animation for puzzle

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,33 @@ module.exports = {
         warning: '#f97316',
         info: '#575958',
         success: '#4ade80'
+      },
+      keyframes: {
+        puzzle: {
+          '0%': {},
+          '100%': {
+            left: '50%',
+            order: 2
+          }
+        },
+        pivotPuzzle: {
+          '0%': {},
+          '100%': {
+            left: '50%',
+            order: 1
+          }
+        },
+        translateX: {
+          '0%': {},
+          '100%': {
+            transform: 'translateX(var(--translateX))'
+          }
+        }
+      },
+      animation: {
+        puzzle: 'puzzle 1s ease-in-out forwards',
+        pivotPuzzle: 'pivotPuzzle 2s ease-in-out forwards',
+        translateX: 'translateX 2s ease-in-out forwards'
       }
     }
   },
