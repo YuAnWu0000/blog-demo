bf8ebba153261f961f8d85a8b1e84d7e8f7d4ad3
zachary <andywu03242000@gmail.com>
2025-02-14 14:43:09 +0800

    UPDATE: Check marquee length

--- src/components/marquee/Marquee.js
+++ src/components/marquee/Marquee.js
@@ -1,4 +1,24 @@
+import { useEffect, useState } from 'react'
+
 function Marquee() {
+  const [marqueeText, setMarqueeText] = useState(
+    '跑馬燈1跑馬燈2跑馬燈3跑馬燈4跑馬燈5跑馬燈6跑馬燈7跑馬燈8跑馬燈9跑馬燈10'
+  )
+  const [marqueeLengthRatio, setMarqueeLengthRatio] = useState(100)
+  const [marqueetime, setMarqueeTime] = useState(0)
+
+  function checkMarquee(el, type) {
+    const clientWidth = el.clientWidth
+    const scrollWidth = el.scrollWidth
+    const ratio = Math.round((scrollWidth / clientWidth) * 100)
+    setMarqueeLengthRatio(ratio)
+    setMarqueeTime(Math.round((ratio + 100) * 10) / 100)
+  }
+
+  useEffect(() => {
+    checkMarquee()
+  }, [marqueeText])
+
   return (
     <div
       className="relative top-[10rem] left-[30rem] right-[30rem] w-[calc(100vw-60rem)] h-[4.2rem]
@@ -7,9 +27,7 @@ function Marquee() {
         [line-height:4.2rem] text-4xl overflow-hidden translate-x-0 border-gray-800
         bg-gray-200"
     >
-      <p className="w-full animate-runMarquee">
-        跑馬燈1跑馬燈2跑馬燈3跑馬燈4跑馬燈5跑馬燈6跑馬燈7跑馬燈8跑馬燈9跑馬燈10
-      </p>
+      <p className="w-full animate-runMarquee"></p>
     </div>
   )
 }
